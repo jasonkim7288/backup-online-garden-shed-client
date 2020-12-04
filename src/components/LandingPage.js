4ff44fa94d39ba0719959a260a0e2df7241c7dbd
Thomas Doig <thomasdoig@hotmail.com>
2020-12-04 22:18:47 +1000

    Basic grid layout for landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import '../styles/landing-page.css'
 
 const GardenShed = () => {
   return (
-    <div>
-      Landing page
+    <div className="wrapper">
+      <div className="item">1</div>
+      <div className="item">2</div>
+      <div className="item">3</div>
+      <div className="item">4</div>
+      <div className="item">5</div>
+      <div className="item">6</div>
+      <div className="item">
+        <button type="button" className="block">Guest</button>
+      </div>
+      <div className="item">8</div>
+      <div className="item">
+        <button type="button" className="block">Sign in with Google</button>
+      </div>
+      <div className="item">10</div>
+      <div className="item">11</div>
+      <div className="item">12</div>
+      <div className="item">13</div>
+      <div className="item">14</div>
+      <div className="item">15</div>
     </div>
   )
 }
