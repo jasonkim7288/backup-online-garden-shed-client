5c1682ed16a121fe6633bb9d0527da721297aa64
Thomas Doig <thomasdoig@hotmail.com>
2020-12-07 17:03:48 +1000

    Continued to setup grid layouts, and changed the css to 1 file in public

---
+++ src/components/GardenSheds.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+const GardenSheds = () => {
+  return (
+    <div>
+      <h1>Garden Sheds</h1>
+      <div id="garden-sheds-container">
+        <div>
+          <img className="garden-shed" src="http://placekitten.com/800/400" alt=""/>
+          <p>some email</p>
+          <p>Daisy, Sun flower, Peace Lilly</p>
+        </div>
+        <div>
+          <img className="garden-shed" src="http://placekitten.com/800/400" alt=""/>
+          <p>some email</p>
+          <p>Daisy, Sun flower, Peace Lilly</p>
+        </div>
+        <div>
+          <img className="garden-shed" src="http://placekitten.com/800/400" alt=""/>
+          <p>some email</p>
+          <p>Daisy, Sun flower, Peace Lilly</p>
+        </div>
+        <div>
+          <img className="garden-shed" src="http://placekitten.com/800/400" alt=""/>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div> 
+  )
+}
+
+export default GardenSheds
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,28 +1,15 @@
 import React from 'react'
-import '../styles/landing-page.css'
+
 
 const GardenShed = () => {
   return (
     <div className="landing-page-container">
-      <div className="lpc-item">1</div>
-      <div className="lpc-item">2</div>
-      <div className="lpc-item">3</div>
-      <div className="lpc-item">4</div>
-      <div className="lpc-item">5</div>
-      <div className="lpc-item">6</div>
-      <div className="lpc-item">
+      <div className="guest-login-item">
         <button type="button" className="block">Guest</button>
       </div>
-      <div className="lpc-item">8</div>
-      <div className="lpc-item">
+      <div className="google-login-item">
         <button type="button" className="block">Sign in with Google</button>
       </div>
-      <div className="lpc-item">10</div>
-      <div className="lpc-item">11</div>
-      <div className="lpc-item">12</div>
-      <div className="lpc-item">13</div>
-      <div className="lpc-item">14</div>
-      <div className="lpc-item">15</div>
     </div>
   )
 }
