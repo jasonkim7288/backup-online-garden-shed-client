e1d56b172358ba706ed49532564e00db2484a0da
Thomas Doig <thomasdoig@hotmail.com>
2020-12-09 06:59:39 +1000

    fixed layout issues with plant record page and side margins

--- src/components/PlantRecord.js
+++ src/components/PlantRecord.js
@@ -4,33 +4,33 @@ const PlantRecord = () => {
   return (
     <div>
       <h1>Plant Record</h1>
+      
+      <div className="button">
+          <button type="button new-log">Creat a new log</button>
+        </div>
+        <div className="button">
+          <button type="button about">About</button>
+        </div>
+
       <div id="plant-record-container">
-
-        <div className="button">
-          <button id="new-log" type="button">Creat a new log</button>
-        </div>
-        <div className="button">
-          <button id="about" type="button about">About</button>
-        </div>
-
         <div id="image-wrapper">
           <img class="main-image" src="http://placekitten.com/1000/600"  alt=""/>
         </div>
 
-        <div className="thumbnail thumbnail-1">
-          <img src="http://placekitten.com/70/50" width="70" alt=""/>
+        <div className="thumbnail-1">
+          <img className="thumbnail" src="http://placekitten.com/70/50" alt=""/>
         </div>
-        <div className="thumbnail thumbnail-2">
-          <img src="http://placekitten.com/70/50" width="70" alt=""/>
+        <div className="thumbnail-2">
+          <img className="thumbnail" src="http://placekitten.com/70/50" alt=""/>
         </div>
-        <div className="thumbnail thumbnail-3">
-          <img src="http://placekitten.com/70/50" width="70" alt=""/>
+        <div className="thumbnail-3">
+          <img className="thumbnail" src="http://placekitten.com/70/50" alt=""/>
         </div>
-        <div className="thumbnail thumbnail-4">
-          <img src="http://placekitten.com/70/50" width="70" alt=""/>
+        <div className="thumbnail-4">
+          <img className="thumbnail" src="http://placekitten.com/70/50" alt=""/>
         </div>
-        <div className="thumbnail thumbnail-5">
-          <img src="http://placekitten.com/70/50" width="70" alt=""/>
+        <div className="thumbnail-5">
+          <img className="thumbnail" src="http://placekitten.com/70/50" alt=""/>
         </div>
 
       </div>
