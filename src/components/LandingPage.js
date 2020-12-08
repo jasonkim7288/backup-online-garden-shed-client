f52a327840aa744649d7c681ac41b099961b5adc
Thomas Doig <thomasdoig@hotmail.com>
2020-12-08 10:58:13 +1000

    postioning the Sign in buttons for each device

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,11 +4,11 @@ import React from 'react'
 const GardenShed = () => {
   return (
     <div className="landing-page-container">
-      <div className="guest-login-item">
-        <button type="button" className="block">Guest</button>
+      <div className="guest-login-item" >
+        <button type="button" className="button guest">Guest</button>
       </div>
       <div className="google-login-item">
-        <button type="button" className="block">Sign in with Google</button>
+        <button type="button" className="button google">Sign in with Google</button>
       </div>
     </div>
   )
