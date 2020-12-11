b80a148a732d6ba20a3f0253df0c05f36fe45899
Thomas Doig <thomasdoig@hotmail.com>
2020-12-11 21:30:11 +1000

    Cleaned up Plant Record grid

--- src/components/PlantRecord.js
+++ src/components/PlantRecord.js
@@ -5,16 +5,16 @@ const PlantRecord = () => {
     <div>
       <h1>Plant Record</h1>
       
-      <div className="button">
-          <button type="button new-log">Creat a new log</button>
+      <div className="button-wrapper">
+          <button className="new-log" type="button new-log">Creat a new log</button>
         </div>
-        <div className="button">
-          <button type="button about">About</button>
+        <div className="button-wrapper">
+          <button className="about" type="button about">About</button>
         </div>
 
       <div id="plant-record-container">
         <div id="image-wrapper">
-          <img class="main-image" src="http://placekitten.com/1000/600"  alt=""/>
+          <img class="main-image" src="http://placekitten.com/400/400"  alt=""/>
         </div>
 
         <div className="thumbnail-1">
@@ -34,6 +34,16 @@ const PlantRecord = () => {
         </div>
 
       </div>
+        <h1>Date:</h1> 17/11/2020 (Day 1)
+        <h1>My Notes:</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec semper vitae magna eget mattis. 
+          Vivamus posuere iaculis consequat. Pellentesque euismod elementum bibendum.
+          Quisque tincidunt nisi a ligula sagittis accumsan. Fusce sed luctus elit. 
+          Aenean at ipsum iaculis, facilisis lectus et, interdum nisi. Pellentesque quis fermentum ante. 
+          In id massa eu nisi dignissim elementum in vitae quam. Duis eros ante, pulvinar ut est et, maximus consequat augue. 
+          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed vitae tellus lacus. 
+        </p>
     </div>
   )
 }
