bf0dfd990fa9578733ac7382cb77750d3a6925fb
Thomas Doig <thomasdoig@hotmail.com>
2020-12-15 10:21:35 +1000

    displayed user email within selected garden shed (plantThumbnails)

--- src/components/PlantThumbnails.js
+++ src/components/PlantThumbnails.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useGlobalState } from '../config/globalState';
+import api from '../config/api';
 
 
 const PlantThumbnails = () => {
+  const { state, dispatch } = useGlobalState();
+  const { sheds } = state;
+  const [shed, setShed] = useState(null); 
   const { shedId } = useParams();
   console.log(shedId);
+  useEffect(() => {
+    // const foundShed = sheds.find(element => element._id === shedId);
+    const findShed = async () => {
+      const res = await api(`/api/sheds/${shedId}`); 
+      const foundShed = res.data;
+      console.log('foundShed:', foundShed);
+      if(foundShed) {
+        setShed(foundShed);
+      } 
+    }
+    findShed();
+  }, []);
+  console.log('shed:', shed);
   return (
     <div>
+      {shed && shed.owner.email}
       <h1>Plant Thumbnails</h1>
       <div id="plant-thumbnails-container">
         <div className="plant-thumbnail-wrapper">
