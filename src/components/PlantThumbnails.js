be4e1e80a066368bd55688f14331ead213c2b2ce
Thomas Doig <thomasdoig@hotmail.com>
2020-12-16 11:10:42 +1000

    linked plant thumbnails to the database

--- src/components/PlantThumbnails.js
+++ src/components/PlantThumbnails.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useGlobalState } from '../config/globalState';
 import api from '../config/api';
 
@@ -9,16 +9,15 @@ const PlantThumbnails = () => {
   const { sheds } = state;
   const [shed, setShed] = useState(null); 
   const { shedId } = useParams();
-  console.log(shedId);
   useEffect(() => {
     // const foundShed = sheds.find(element => element._id === shedId);
     const findShed = async () => {
-      const res = await api(`/api/sheds/${shedId}`); 
+      const res = await api(`/api/sheds/${shedId}`);
       const foundShed = res.data;
       console.log('foundShed:', foundShed);
       if(foundShed) {
         setShed(foundShed);
-      } 
+      }
     }
     findShed();
   }, []);
@@ -29,12 +28,19 @@ const PlantThumbnails = () => {
         shed &&
           <>
             <p className="path">{shed.owner.email}</p>
-          
             <div id="plant-thumbnails-container">
-              <div className="plant-thumbnail-wrapper">
-                <img className="plant-thumbnail" src="http://placekitten.com/400/300" alt=""/>
-                <p>Daisy 1</p>
-              </div>
+              {shed.plantRecords.map(plantRecord => (
+                <Link to={`/sheds/${shedId}/records/${plantRecord._id}`}>
+                  <div className="plant-thumbnail-wrapper">
+                    <img className="plant-thumbnail" src={plantRecord.recordPhoto} alt=""/>
+                    <p>{plantRecord.commonName}</p>
+                  </div>
+                </Link>
+              ))}
+            </div>
+            <p className="path">{shed.owner.email}</p>
+
+            <div id="plant-thumbnails-container">
               <div className="plant-thumbnail-wrapper">
                 <img className="plant-thumbnail" src="http://placekitten.com/400/300" alt=""/>
                 <p>Daisy 2</p>
--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: 'https://garden-shed.herokuapp.com',
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://garden-shed.herokuapp.com',
+  // baseURL: 'http://localhost:8080',
   timeout: 10000,
   withCredentials: true
 })
