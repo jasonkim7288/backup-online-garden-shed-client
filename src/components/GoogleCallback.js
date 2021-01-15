db665889a5052e3473b3ce2174eec72f3f4d9962
jasonkim7288 <jason.kim7288@gmail.com>
2021-01-15 16:01:42 +1000

    remove warning

--- src/components/GoogleCallback.js
+++ src/components/GoogleCallback.js
@@ -1,26 +1,6 @@
-import React, { useEffect } from 'react';
-import { useHistory, useLocation, useParams } from 'react-router-dom';
+import React from 'react';
 
 const GoogleCallback = () => {
-  const param = useParams();
-  let location = useLocation();
-  let history = useHistory();
-  console.log('param:', param);
-  console.log('location:', location);
-  console.log('param.id_token:', param.id_token);
-
-  useEffect(() => {
-    const locationHash = location.hash.substr(1)
-    if (locationHash === '') {
-      history.push('/');
-    } else {
-      const id_token = locationHash.substr(locationHash.search(/(?<=^|&)id_token=/))
-        .split('&')[0]
-        .split('=')[1];
-      console.log('id_token:', id_token);
-    }
-  }, []);
-
   return (
     <>
       google callback
