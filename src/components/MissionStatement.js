714af55839bb339ebb55528aa259507cf33aa6de
Thomas Doig <thomasdoig@hotmail.com>
2021-01-17 21:37:26 +1000

    Left align mission statement text, edit landing page

--- src/components/MissionStatement.js
+++ src/components/MissionStatement.js
@@ -5,16 +5,16 @@ const MissionStatement = () => {
     <div>
       <h1 className="title">Mission Statement</h1>
       <img id="mission-statement-image" src="/missionStatement.jpg" alt="cartoon gardeners'"></img>
-      <p id="mission-statement">
+      <p className="mission-statement">
         Started from within Australia, Online Garden Shed is for improving your gardening skills by 
         keeping track of a plant's growth history and learning from other gardeners' experiences. 
         It also provides a way for gardeners to be a part of a much bigger community by bringing their 
         records and experiences online. By having access to the online garden sheds and being able to 
         follow other plant records and setting up notifications, gardeners can be more actively involved 
         in their gardening community whilst not being restricted by their geographic location.
-        <br/>
-        <br/>
-        Lets help each other and the planet.
+        </p>
+        <p className="mission-statement last-line">
+          Lets help each other and the planet.
         </p>
     </div>
   );
