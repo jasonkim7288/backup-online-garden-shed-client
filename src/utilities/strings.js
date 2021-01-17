4f6a92c462362326ea4a351408c6e9043da45f2f
jason.kim7288 <jason.kim7288@gmail.com>
2021-01-17 21:34:00 +1000

    add cypress test

--- src/utilities/strings.js
+++ src/utilities/strings.js
@@ -1,11 +1,19 @@
 export const removeDomain = (email) => {
-  return email.split('@')[0];
+  if (email && typeof email === 'string') {
+    return email.split('@')[0];
+  } else {
+    return '';
+  }
 }
 
 export const getUniquePlantName = (plantRecord) => {
-  if (plantRecord.recordNum > 1) {
-    return `${plantRecord.commonName} (${plantRecord.recordNum})`;
+  if (plantRecord) {
+    if (plantRecord.recordNum > 1) {
+      return `${plantRecord.commonName} (${plantRecord.recordNum})`;
+    } else {
+      return plantRecord.commonName;
+    }
   } else {
-    return plantRecord.commonName;
+    return '';
   }
 }
